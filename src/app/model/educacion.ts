daac31789070136f2d2818f00b99e007a3d1f66a
Matias <albanesigaston@gmail.com>
2022-08-07 21:23:17 -0300

    implementacion de botones editar agregar y borrar

--- src/app/model/educacion.ts
+++ src/app/model/educacion.ts
@@ -2,12 +2,14 @@ export class Educacion {
     id?: number;
     institucion: string;
     titulo: string;
-    desc: string;
+    descripcion: string;
+    ruta: string;
 
-    constructor(institucion: string, titulo: string, desc: string){
+    constructor(institucion: string, titulo: string, descripcion: string, ruta: string){
         this.institucion = institucion;
         this.titulo = titulo;
-        this.desc = desc;
+        this.descripcion = descripcion;
+        this.ruta = ruta;
     }
 
 }
